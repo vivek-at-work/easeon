9c7d2b830728bf087a0523462805a43f94ca061d
VivekDSrivastava <developer.viveksrivastava@gmail.com>
2020-05-14 14:23:44 +0530

    updated validation for Holiday serializer

--- EaseOn/apps/organizations/serializers/holiday.py
+++ EaseOn/apps/organizations/serializers/holiday.py
@@ -7,6 +7,15 @@ from rest_framework import serializers
 class HolidaySerializer(BaseSerializer):
     """ """
 
+    def validate(self, data):
+        """
+        Check that if record already exists..
+        """
+        count = Holiday.objects.filter(organization=data['organization'],date=data['data']).count()
+        if count:
+            raise serializers.ValidationError("Holiday Details Already Exists")
+        return data
+
     class Meta(BaseMeta):
         model = Holiday
         fields = (
