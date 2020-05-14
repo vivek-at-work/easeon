53f56809cdbc1a99f84f1764211dd230f3099901
VivekDSrivastava <developer.viveksrivastava@gmail.com>
2020-05-14 14:52:18 +0530

    updates refresh gsx_token to reresh for all similier gsxusernames

--- EaseOn/apps/core/gsx/gsx_request.py
+++ EaseOn/apps/core/gsx/gsx_request.py
@@ -111,10 +111,8 @@ class GSXRequest:
         output = response.read()
         result = json.loads(output) if is_json(output) else output
         if 'authToken' in result:
-            if not user:
-                user = get_user_model().objects.filter(
-                    gsx_user_name=self.gsx_user_name
-                ).update(gsx_auth_token=result['authToken'],gsx_token_last_refreshed_on=timezone.now())
+            get_user_model().objects.filter(
+                    gsx_user_name=self.gsx_user_name).update(gsx_auth_token=result['authToken'],gsx_token_last_refreshed_on=timezone.now())
             self.auth_token = result['authToken']
             return True
         return False
